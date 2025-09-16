6e2d63b42689d9c150849fcd82a40c9932d22b91
sjz <1435608435@qq.com>
2025-09-16 11:01:07 +0800

    ✨ feat: load adapters from repo

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log/slog"
 
 	"github.com/meloshub/meloshub/adapter"
 	"github.com/meloshub/meloshub/logging"
+
+	_ "github.com/meloshub/meloshub-adapters/all" //加载仓库所有可用适配器
 )
 
 func main() {
@@ -20,10 +24,28 @@ func main() {
 		slog.Warn("Adapters list is empty")
 	}
 	for _, v := range adapters {
-		println(v.Id())
+		fmt.Printf("adapter：%s is available", v.Id())
 	}
-	_, ok := adapter.Get("example")
+
+	// 获取指定的适配器并使用
+	qqmusic, ok := adapter.Get("qqmusic")
 	if !ok {
-		slog.Error("adapter 'example' is not existed")
+		slog.Error("adapter 'qqmusic' is not existed")
 	}
+
+	songList, err := qqmusic.Search("夜的第七章", adapter.SearchOptions{
+		Page:  1,
+		Limit: 10,
+	})
+	if err != nil {
+		slog.Error(err.Error())
+		return
+	}
+
+	jsonBytes, err := json.MarshalIndent(songList, "", "  ")
+	if err != nil {
+		fmt.Println("JSON marshaling failed:", err)
+		return
+	}
+	fmt.Println(string(jsonBytes))
 }
